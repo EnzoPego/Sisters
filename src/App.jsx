f678628b2769287f73ef041e18c0a0bf1cc9730c
unknown <enzzo2410@gmail.com>
2023-09-02 17:00:55 -0300

    se agregan los componentes Navbar,Videodisplay y Contacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import React from 'react';
+import { NavBar } from './components/NavBar/NavBar';
+import { Contacto } from './components/Contacto/Contacto';
+import { VideoDisplay } from './components/VideoDisplay/VideoDisplay';
+
 
 export const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+      <NavBar  /> 
+      <VideoDisplay /> 
+      <Contacto />    
+    </>
+  );
 }
-
